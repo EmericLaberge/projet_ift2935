76428a4715cfa57842a1d6f5c9922ca0efd614f1
EmericLaberge <102610334+EmericLaberge@users.noreply.github.com>
2024-03-28 00:16:28 -0400

    Tailwind setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import SignInSide from './SignInSide';
 import Dashboard from './templates/dashboard/Dashboard';
+import Button from '@mui/material/Button/Button';
+
 
 
 function App() {
   return (
     <div className="App">
     <Dashboard />
+    <Button variant="contained" color="primary" className="btn mt-2">Hello World</Button>
     </div>
   );
 }
